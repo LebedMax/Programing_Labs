259098b952a351983959b4e72e95f2ed4287bf09
LebedMax <71449102+LebedMax@users.noreply.github.com>
2020-09-17 18:07:31 +0300

    Add files via upload

---
+++ Lab-1/Lab-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            double r=0;
+            Console.WriteLine( " Enter value n ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value r ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double P = 2 * r * n * Math.Tan((Math.PI / n));
+            Console.Write(P);
+        }
+	}
+}
